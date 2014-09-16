45fecdbb69c5a2f5ef7459a1f0545ffff17a5867
JOSE NARANJO <joselnaranjo@gmail.com>
2014-09-15 20:59:34 -0500

    Loading data from service.

--- app/elements/py-table/py-table.js
+++ app/elements/py-table/py-table.js
@@ -2,35 +2,20 @@
   'use strict';
   Polymer({
     created: function() {
-      this.tableContent = [
-      {
-        id: 1,
-        title: 'this is the title',
-        subject: 'this is a subject',
-        descritiopn: 'this is a description'
-      },
-      {
-        id: 2,
-        title: 'this is the title 2',
-        subject: 'this is a subject 2',
-        descritiopn: 'this is a description 2'
-      },
-      {
-        id: 3,
-        title: 'this is the title 3',
-        subject: 'this is a subject 3',
-        descritiopn: 'this is a description 3'
-      }]; // Initialize and hint type to an object.
-
-      this.headers = this.getHeaders();
+      this.tableContent = []; // Initialize and hint type to an object.
     },
 
-    getHeaders : function() {
+    getHeaders: function() {
       var headers = [];
       if(this.tableContent.length > 0) {
         headers = Object.getOwnPropertyNames(this.tableContent[0]);
       }
       return headers;
+    },
+    
+    ajaxResponse: function(event, data) {
+      this.tableContent = data.response;
+      this.headers = this.getHeaders();
     }
   });
 })();
