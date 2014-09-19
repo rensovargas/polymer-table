ced38f8127ccb71e63783b13e82b9fbbb8e858b6
Renso Vargas <rensovargas@gmail.com>
2014-09-18 22:08:58 -0500

    getAlphaIndex function added to retrieve the alphabetical index based on an integer

--- app/elements/py-cell/py-cell.js
+++ app/elements/py-cell/py-cell.js
@@ -1,6 +1,34 @@
 (function () {
   'use strict';
   Polymer({
-//        cellData: 'Empty'
-  });
+    
+    created: function() {
+      //this.columns = this.getColumns();
+    },
+    
+    // TODO: Extract method to a helper
+    getAlphaIndex: function(index){
+        
+        var columnBase = 26;
+        var digitMax = 7; // ceil(log26(Int32.Max))
+        var digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+        if (index <= 0){
+          throw new Error('index must be a positive number');
+        }
+
+        if (index <= columnBase){
+          return digits[index - 1].toString();
+        }
+
+        var sb = [];
+        var current = index;
+        var offset = digitMax;
+        while (current > 0){
+            sb[--offset] = digits[--current % columnBase];
+            current = parseInt(current / columnBase);
+        }
+        return sb.join('');
+    }
+});
 })();
--- app/elements/py-table/py-table.js
+++ app/elements/py-table/py-table.js
@@ -1,36 +1,58 @@
 (function () {
   'use strict';
   Polymer({
-    created: function() {
-      this.tableContent = [
+    data : [
       {
         id: 1,
         title: 'this is the title',
         subject: 'this is a subject',
-        descritiopn: 'this is a description'
+        description: 'this is a description'
       },
       {
         id: 2,
         title: 'this is the title 2',
         subject: 'this is a subject 2',
-        descritiopn: 'this is a description 2'
+        description: 'this is a description 2'
       },
       {
         id: 3,
         title: 'this is the title 3',
         subject: 'this is a subject 3',
-        descritiopn: 'this is a description 3'
-      }]; // Initialize and hint type to an object.
-
-      this.headers = this.getHeaders();
+        description: 'this is a description 3'
+      }],
+    created: function() {
+      this.columns = this.getColumns();
+      
     },
 
-    getHeaders : function() {
+    getColumns : function() {
       var headers = [];
-      if(this.tableContent.length > 0) {
-        headers = Object.getOwnPropertyNames(this.tableContent[0]);
+      var columns = {};
+      if(this.data.length > 0) {
+        headers = Object.keys(this.data[0]);
+        for(var i in headers){
+          columns[headers[i]] = parseInt(i) + 1;
+        }
       }
-      return headers;
+      
+      return columns;
+    },
+    
+    // TODO: Extract method to a helper
+    keys: function(object) {
+      return Object.keys(object);
+    },
+    
+    index: function(element, array){
+      return array.indexOf(element) + 1;
+    },
+    
+    foo: function(array){
+      return array.length;
+    },
+    
+    debug: function(object){
+      console.log("debug", object);
     }
   });
 })();
